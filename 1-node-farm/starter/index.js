55a285adb440f6c2e01188fbcc1ccdd572af92eb
Sushant Sharma <ssushantlb08@gmail.com>
2022-10-01 23:43:23 +0530

    async read file

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -10,3 +10,11 @@ console.log(file);
 // writing a file
 const fileWrite = fs.writeFileSync('./txt/written.txt' , `hello there today we'll loook at avacado's : ${file} , created on : ${Date.now()}`);
 console.log(fs.readFileSync('./txt/written.txt', 'utf-8'));
+
+// async read file
+console.log(`-------------X-------------X-------------X-------------X-------------X-------------X-------------X-------------`);
+
+fs.readFile('./txt/start.txt' , 'utf-8' , (err,data) =>{
+    console.log(err , data);
+});
+console.log('this should be read first');
