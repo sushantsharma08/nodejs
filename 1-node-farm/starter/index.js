30263cb072a1931884af76a96448180b59b944a4
Sushant Sharma <ssushantlb08@gmail.com>
2022-10-01 23:53:48 +0530

    async wrte file

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -15,6 +15,20 @@ console.log(fs.readFileSync('./txt/written.txt', 'utf-8'));
 console.log(`-------------X-------------X-------------X-------------X-------------X-------------X-------------X-------------`);
 
 fs.readFile('./txt/start.txt' , 'utf-8' , (err,data) =>{
-    console.log(err , data);
+    console.log( data);
 });
 console.log('this should be read first');
+
+
+// reading a file from result of other 
+fs.readFile('./txt/start.txt' , 'utf-8' , (err,data1) =>{
+    fs.readFile(`./txt/${data1}.txt` , 'utf-8' , (err,data2) =>{
+        console.log( data2);
+        fs.readFile('./txt/append.txt' , 'utf-8' , (err,data3) =>{
+            console.log(data3);
+            fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,'utf-8', err=>{
+                console.log('file written');
+            })
+        });
+    })
+});
