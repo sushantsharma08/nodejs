7b7aeba10f7f9c36a944435e620f0fe94a5660e1
Sushant Sharma <ssushantlb08@gmail.com>
2022-10-12 23:46:33 +0530

    modules basics

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const replaceTemplate=require('./modules/replaceTemplate')
 // const hello ="Hello World";
 
 
@@ -43,19 +44,7 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
 const tempOverview =  fs.readFileSync(`${__dirname}/templates/overview.html`,'utf-8');
 const tempCard =  fs.readFileSync(`${__dirname}/templates/card.html`,'utf-8');
 const tempProduct =  fs.readFileSync(`${__dirname}/templates/product.html`,'utf-8');
-const replaceTemplate = (temp,product)=>{
-    let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName)
-    output = output.replace(/{%PRODUCTNAME%}/g,product.productName)
-    output = output.replace(/{%ID%}/g,product.id)
-    output = output.replace(/{%IMAGE%}/g,product.image)
-    output = output.replace(/{%PRICE%}/g,product.price)
-    output = output.replace(/{%FROM%}/g,product.from)
-    output = output.replace(/{%NUTRIENTS%}/g,product.nutrients)
-    output = output.replace(/{%QUANTITY%}/g,product.quantity)
-    output = output.replace(/{%DESCRIPTION%}/g,product.description)
-    if(!product.organic)output = output.replace(/{%NOT_ORGANIC%}/g,'not-organic')
-    return output;
-}
+
 
 const dataObj = JSON.parse(data)
 const port = 8000;
