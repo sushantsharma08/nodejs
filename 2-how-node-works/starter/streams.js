021b18bbb816f95c203e5d364aac473b1f6e8f70
Sushant Sharma <ssushantlb08@gmail.com>
2022-11-06 23:39:40 +0530

    Update streams.js

--- 2-how-node-works/starter/streams.js
+++ 2-how-node-works/starter/streams.js
@@ -3,10 +3,14 @@ const fs = require('fs')
 const server = require('http').createServer();
 
 server.on("request",(req,res)=>{
-    fs.readFile("test-file.txt",(err,data)=>{
-        if(err) console.log("error reading file");
-        res.end(data);
-    })
+    /* soln 1 */
+    // fs.readFile("test-file.txt",(err,data)=>{
+    //     if(err) console.log("error reading file");
+    //     res.end(data);
+    // })
+    // soln 2
+    const readable = fs.createReadStream('test-file.txt');
+    readable.pipe(res);
 })
 
 server.listen(4000 , '127.0.0.1',()=>{
