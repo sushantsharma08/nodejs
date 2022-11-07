51d3978511c819f3cc0b0732b7c6f0a596847b65
Sushant Sharma <ssushantlb08@gmail.com>
2022-11-07 23:43:39 +0530

    intro to express

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -2,7 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.get('/')
+app.get('/',(req,res)=>{
+    res
+    .status(200)
+    .json({message:'Hello from server side', app:'Natours'});
+});
+
+app.post('/',(req,res)=>{
+    res.send('You can post to this input')
+})
+
 
 const port =3000
 app.listen(port,()=>{
