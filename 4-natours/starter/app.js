4074906a8de1fee44f54e55dca6ce6516c40ac39
Sushant Sharma <ssushantlb08@gmail.com>
2022-11-08 14:29:05 +0530

    get meathod

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,18 +1,28 @@
 const express = require('express');
-
+const fs = require('fs');
 const app = express();
+// app.get('/',(req,res)=>{
+//     res
+//     .status(200)
+//     .json({message:'Hello from server side', app:'Natours'});
+// });
 
-app.get('/',(req,res)=>{
+// app.post('/',(req,res)=>{
+//     res.send('You can post to this input  ')
+// })
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+
+app.get('/api/v1/tours',(req,res)=>{
     res
     .status(200)
-    .json({message:'Hello from server side', app:'Natours'});
-});
-
-app.post('/',(req,res)=>{
-    res.send('You can post to this input')
+    .json({
+        length : tours.length,
+        status:'success',
+        data: {
+            tours:tours
+        }
+    })
 })
-
-
 const port =3000
 app.listen(port,()=>{
     console.log(`running on port ${port}...`);
