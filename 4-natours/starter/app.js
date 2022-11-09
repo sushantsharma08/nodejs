e2280a9f2dd73b08ba27ef89b01d88aebd60cd62
Sushant Sharma <ssushantlb08@gmail.com>
2022-11-09 22:46:48 +0530

    url parameters

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+app.use(express.json());
 // app.get('/',(req,res)=>{
 //     res
 //     .status(200)
@@ -12,18 +13,51 @@ const app = express();
 // })
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
-app.get('/api/v1/tours',(req,res)=>{
+app.get('/api/v1/tours', (req, res) => {
     res
-    .status(200)
-    .json({
-        length : tours.length,
+        .status(200)
+        .json({
+            length: tours.length,
+            status: 'success',
+            data: {
+                tours: tours
+            }
+        })
+})
+
+app.get('/api/v1/tours/:id', (req, res) => {
+    console.log(req.params.id);
+
+    const id = Number(req.params.id)
+    const tour = tours.find(el =>el.id===id)
+
+    res.json({
         status:'success',
-        data: {
-            tours:tours
+        data:{
+            tour
         }
     })
+    })
+
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body);
+    const newid = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newid }, req.body);
+
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res
+            .status(201)
+            .json({
+                "status": "success",
+                "data": {
+                    tour: newTour
+                }
+            })
+    })
 })
-const port =3000
-app.listen(port,()=>{
+
+const port = 3000
+app.listen(port, () => {
     console.log(`running on port ${port}...`);
 });
