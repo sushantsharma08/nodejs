f64d1875a11c838c98e1c9f514fbb8bd7212e944
Sushant Sharma <ssushantlb08@gmail.com>
2022-11-12 22:54:44 +0530

    Update app.js

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const app = express();
 app.use(express.json());
 
+app.use(morgan('dev'))
 app.use((req,res,next)=>{
     console.log(`hello from middleware😁`);
     next();
