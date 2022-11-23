fba37af170ba5a2de7d2ed39ae35ac8c201a2f78
Sushant Sharma <ssushantlb08@gmail.com>
2022-11-23 23:46:21 +0530

    connecting mongodb with  mongoose

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,8 +1,23 @@
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 
 dotenv.config({path:'./config.env'})
 const app =require('./app')
 // console.log(process.env);
+
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+    );
+
+mongoose.connect(DB,{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    UseFindAndModify:false
+}).then(con=>{
+    console.log(con.connections);
+    console.log(`DB connection successful`);
+})
 
 const port = process.env.PORT||3000
 app.listen(port, () => {
