0dd37a17ce007b4bf440e4c4fd73876260a617dd
Sushant Sharma <ssushantlb08@gmail.com>
2022-12-01 23:16:57 +0530

    Update server.js

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,27 +1,26 @@
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 
-dotenv.config({path:'./config.env'})
-const app =require('./app')
+dotenv.config({ path: './config.env' })
+const app = require('./app')
 // console.log(process.env);
 
 const DB = process.env.DATABASE.replace(
     '<PASSWORD>',
     process.env.DATABASE_PASSWORD
-    );
+);
 
-mongoose.connect(DB,{
-    useNewUrlParser:true,
-    useCreateIndex:true,
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
     // UseFindAndModify:false
-}).then(con=>{
+}).then(con => {
     // console.log(con.connections);
     console.log(`DB connection successful`);
 })
 
 
-
-const port = process.env.PORT||3000
+const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log(`running on port ${port}...`);
 });
